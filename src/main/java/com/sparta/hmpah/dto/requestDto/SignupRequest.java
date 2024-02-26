f98b2e7d1d511a9b6995e4aa41360f64e7128161
lee doyoen <103111681+noeyodeel@users.noreply.github.com>
2024-02-26 19:34:31 +0900

    Feat/login (#3)
    * :memo: gitignore properties 추가
    
    * :sparkles: login 구현
    
    * :sparkles: 회원가입 구현
    
    * :sparkles: 로그인, 회원가입, 메인페이지 html 구현
    
    * :sparkles: 카카오 소셜 로그인 구현
    
    * :sparkles: 카카오 회원가입 구현

--- src/main/java/com/sparta/hmpah/dto/requestDto/SignupRequest.java
+++ src/main/java/com/sparta/hmpah/dto/requestDto/SignupRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Email;
 
 @Getter
 @Setter
@@ -11,18 +12,21 @@ public class SignupRequest {
 
     @NotBlank
     @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d].{4,10}$",
-            message = "아이디는 최소 4자 이상, 10자 이하로 알파벳 소문자와 숫자로 구성되어야 합니다.")
+        message = "아이디는 최소 4자 이상, 10자 이하로 알파벳 소문자와 숫자로 구성되어야 합니다.")
     private String username;
 
     @NotBlank
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^&*.])(?=.*\\d)[a-zA-Z!@#$%^&*.\\d].{8,15}$",
-            message = "비밀번호는 최소 8자 이상, 15자 이하로 알파벳과 특수문자, 숫자로 구성되어야 합니다.")
+        message = "비밀번호는 최소 8자 이상, 15자 이하로 알파벳과 특수문자, 숫자로 구성되어야 합니다.")
     private String password;
 
-    @NotBlank
     @Pattern(regexp = "^[a-zA-Z0-9가-힣\\d]{2,10}$",
-            message = "닉네임은 2~10자로 구성되어야 합니다.")
+        message = "닉네임은 2~10자로 구성되어야 합니다.")
     private String nickname;
+
+    @Email
+    @NotBlank
+    private String email;
 
     private String profile;
     private boolean admin = false;
