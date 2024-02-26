e6f2d29d21ab81d3c1b0f7d12a308358cc6b2633
LeeJaeHyung <leejh7623@gmail.com>
2024-02-26 20:45:20 +0900

    :sparkles: comment 관련 swagger 추가 CommentController 수정

--- src/main/java/com/sparta/hmpah/controller/CommentController.java
+++ src/main/java/com/sparta/hmpah/controller/CommentController.java
@@ -2,11 +2,11 @@ package com.sparta.hmpah.controller;
 
 import com.sparta.hmpah.dto.requestDto.CommentLikeRequest;
 import com.sparta.hmpah.dto.requestDto.CommentRequest;
-import com.sparta.hmpah.dto.responseDto.CommentLikeResponse;
 import com.sparta.hmpah.dto.responseDto.CommentResponse;
 import com.sparta.hmpah.entity.Comment;
 import com.sparta.hmpah.security.UserDetailsImpl;
 import com.sparta.hmpah.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,11 +30,13 @@ public class CommentController {
   private final CommentService commentService;
 
   @GetMapping("/get-postId")
+  @Operation(summary = "postId를 기준으로 모든 댓글을 조회", description = "postId를 기준으로 모든 댓글을 조회합니다.")
   public List<CommentResponse> getComments(@RequestParam Long postId) {//postId기준 모든 댓글 조회
     return commentService.getComments(postId);
   }
 
   @PostMapping("/create")//댓글 생성
+  @Operation(summary = "댓글 생성", description = "postId와 User를 기준으로 댓글을 생성합니다.")
   public CommentResponse createComment(@RequestBody CommentRequest requestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     log.info(userDetails.getUsername());
@@ -43,23 +45,27 @@ public class CommentController {
   }
 
   @PutMapping("/{id}")//댓글 수정
+  @Operation(summary = "댓글 수정", description = "작성자와 일치하면 commentId를 기준으로 댓글수정합니다.")
   public Comment updateComment(@PathVariable Long id, @RequestBody CommentRequest requestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     return commentService.updateComment(id, requestDto, userDetails);
   }
 
   @DeleteMapping("/{id}")//댓글 삭제
+  @Operation(summary = "댓글 삭제", description = "작성자와 일치하면 commentId를 기준으로 댓글을 삭제합니다.")
   public Long deleteComment(@PathVariable Long id,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     return commentService.deleteComment(id, userDetails);
   }
 
   @GetMapping("/get-like")
+  @Operation(summary = "댓글 좋아요 조회", description = "commentId를 기준으로 댓글을 조회합니다.")
   public Long countByPostId(@RequestParam Long id) {
     return commentService.countByCommentId(id);
   }
 
   @PostMapping("/create-like")//댓글 생성
+  @Operation(summary = "댓글 좋아요 생성", description = "commentId를 기준으로 댓을의 좋아요를 생성합니다.")
   public Long createCommentLike(@RequestBody CommentLikeRequest requestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     log.info("create 호출");
@@ -67,6 +73,7 @@ public class CommentController {
   }
 
   @DeleteMapping("/delete-like/{id}")
+  @Operation(summary = "댓글 좋아요 삭제", description = "commentId를 기준으로 댓을의 좋아요를 삭제합니다.")
   public Long deleteCommentLike(@PathVariable Long id,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     return commentService.deleteCommentLike(id, userDetails);
