c9e0445a258a0dea8756891679527a94266fba35
LeeJaeHyung <69907023+LeeJaeHyung@users.noreply.github.com>
2024-02-27 14:31:25 +0900

    Feat/comment (#4)
    * :sparkles:
    댓글 좋아요 기능 생성
    
    CommentController 수정, CommentService 수정, CommentLikeRepository 작성
    
    * :twisted_rightwards_arrows: dev -> feat/comment
    카카오 로그인 관련 가져와 merge
    
    * :sparkles: comment 관련 swagger 추가
    CommentController 수정
    
    * :sparkles:CommentController에서 UserDetailsImpl을 User로 변경하여 CommentService에 전달
    -> Service단에서는 좀더 비즈니스 로직에 집중이 가능함
    
    CommentService에 내부에서 이용하는 메서드들의 접근제한자를 private로 변경 -> 좀 더 안정적인 로직으로 전환

--- src/main/java/com/sparta/hmpah/repository/CommentRepository.java
+++ src/main/java/com/sparta/hmpah/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package com.sparta.hmpah.repository;
 
 import com.sparta.hmpah.entity.Comment;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+  List<Comment> findByPostIdOrderByCreatedAtAsc(Long postId);
+  void deleteByPostId(Long postId);
 }
